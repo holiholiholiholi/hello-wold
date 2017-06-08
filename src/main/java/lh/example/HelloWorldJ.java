fa943a818d1207436759a4020b814e3cd55a9ec9
holiholiholiholi <holiholiholiholi@gmail.com>
2017-06-08 11:09:33 +0200

    Added maven module

---
+++ src/main/java/lh/example/HelloWorldJ.java
@@ -0,0 +1,10 @@
+package lh.example;
+
+/**
+ * Created by lihong on 07.06.17.
+ */
+public class HelloWorldJ {
+        public static void main(String args[]){
+            System.out.println("HW!");
+        }
+}
---
+++ src/main/scala/lh/example/HelloWorld.scala
@@ -0,0 +1,6 @@
+package lh.example
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world, hahaha !")
+  }
+}
