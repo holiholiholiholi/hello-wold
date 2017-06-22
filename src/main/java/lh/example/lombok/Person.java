a134e94c7e122b049fa53f8c134373e46a23905e
holiholiholiholi <holiholiholiholi@gmail.com>
2017-06-22 15:59:04 +0200

    Added xml conveter test

--- src/main/java/lh/example/lombok/Person.java
+++ src/main/java/lh/example/lombok/Person.java
@@ -5,32 +5,56 @@ package lh.example.lombok;
  */
 
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NonNull;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lh.example.json.LocalDateDeserializer;
+import lh.example.json.LocalDateSerializer;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Data
+@EqualsAndHashCode(exclude={"age","hobbies"})
 @Slf4j
+@JacksonXmlRootElement(localName="person")
 public class Person {
 
     public enum Gender {FEMALE, MALE}
 
-
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private final LocalDate birthday;
 
     @NonNull
+    @JacksonXmlProperty(isAttribute=true)
     private String lastname, surname;
 
     @Setter(AccessLevel.NONE) //disable the setter
+    @JsonIgnore
     private short age;
 
     private Gender gender;
 
-    public void print(){
+    @Setter(AccessLevel.NONE)
+    @JacksonXmlElementWrapper(localName = "hobbies",useWrapping=true)
+    @JacksonXmlProperty(localName = "hobby")
+    private final ArrayList<String> hobbies = new ArrayList();
+
+    public Person(@JsonProperty("birthday") LocalDate birthday, @JsonProperty("lastname") @NonNull String lastname,
+                  @JsonProperty("surname") @NonNull String surname) {
+        this.birthday = birthday;
+        this.lastname = lastname;
+        this.surname = surname;
+    }
+
+    public void print() {
         log.info(toString());
     }
 
--- src/test/java/lh/example/lombok/PersonTest.java
+++ src/test/java/lh/example/lombok/PersonTest.java
@@ -1,9 +1,14 @@
 package lh.example.lombok;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 
@@ -30,8 +35,40 @@ public class PersonTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void test2() throws NullPointerException{
+    public void test2() {
         thrown.expect(IllegalArgumentException.class);
         Person p = new Person(LocalDate.of(2000,1,1), "White",null);
+    }
+
+    @Test
+    public void jsonTest() throws IOException{
+        Person p = new Person(LocalDate.of(2000,1,1), "White","Peter");
+        p.getHobbies().add("A");
+        p.getHobbies().add("B");
+        p.getHobbies().add("C");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        String json = mapper.writeValueAsString(p);
+        System.out.println(json);
+
+        mapper.readValue(json, Person.class).print();
+    }
+
+    @Test
+    public void xmlTest() throws IOException {
+        Person p = new Person(LocalDate.of(2000,1,1), "White","Peter");
+        p.getHobbies().add("A J");
+        p.getHobbies().add("B");
+        p.getHobbies().add("C");
+
+        XmlMapper mapper = new XmlMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String xml = mapper.writeValueAsString(p);
+        System.out.println(xml);
+
+        mapper.readValue(xml, Person.class).print();
     }
 }
